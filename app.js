b7f2ed26a7a6dc46db56f86770f9e4be60ed1ba9
SirMorfield <joppe.koers@gmail.com>
2018-09-13 21:52:14 +0200

    3

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
