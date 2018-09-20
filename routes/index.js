d4157e54ab195fb54d9428f0a18958dd4d178c0a
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2018-09-20 20:43:32 +0200

    sd

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+
+// const fs = require('fs');
+const path = require('path');
+// const home = fs.readFileSync(path.join(__dirname, '../public/contents/home.html'));
+
 let router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index.ejs', { title: 'joppekoers.nl' });
+  res.render('index.ejs', {
+    title: 'joppekoers.nl',
+    mid1: path.join(__dirname, '../public/contents/home.html')
+  });
 });
 
 module.exports = router;
