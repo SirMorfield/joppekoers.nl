7be903b268df4b1ceaa7b82b0e65ce3caaae3c17
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2018-09-22 19:43:55 +0200

    s

---
+++ routes/language.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+
+let router = express.Router();
+router.get('/', (req, res) => {
+  res.render(path.join(__dirname, '../public/views/language.ejs'))
+})
+module.exports = router;
