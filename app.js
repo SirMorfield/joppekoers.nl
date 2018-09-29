87b6f3ec16fdee304052f94db4ac32131ad0be65
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2018-09-29 19:08:55 +0200

    s

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ app.use(compression());
 app.use(favicon(path.join(__dirname, 'public/logo/favicon.ico')))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'public/views'));
+app.set('views', path.join(__dirname, 'views/'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public/')));
 
 
-app.use('/', require('./routes/language.js'));
-app.use('/en/*', require('./routes/index.js'));
-app.use('/nl/*', require('./routes/index.js'));
+app.use('/', require('./routes/index.js'));
+// app.use('/*', require('./routes/index.js'));
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
