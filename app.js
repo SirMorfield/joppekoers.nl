a89fd4cbd6d238fe21eed47a6709cea84297526e
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2018-10-12 17:24:46 +0200

    deletthis

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const logger = require('morgan');
 const favicon = require('serve-favicon')
 const compression = require('compression')
 
+const upload = require('express-fileupload');
+
 let app = express();
+app.use(upload());
 app.use(compression());
 app.use(favicon(path.join(__dirname, 'public/logo/favicon.ico')))
 
@@ -33,11 +36,8 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
---
+++ routes/test1.js
@@ -0,0 +1,9 @@
+const add = (int) => {
+  console.log(int * 2);
+}
+
+
+
+// kljdskfjksjf
+
+module.exports.add = add;
