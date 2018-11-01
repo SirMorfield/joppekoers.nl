001ce13d93dc61f655eacb587e4aac24a86d7ccd
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2018-11-01 20:33:19 +0100

    lmao

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-
 const fs = require('fs');
 const path = require('path');
 const home = fs.readFileSync(path.join(__dirname, '../views/partials/home.ejs'));
 const projects = fs.readFileSync(path.join(__dirname, '../views/partials/projects.ejs'));
+const express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res, next) => {
