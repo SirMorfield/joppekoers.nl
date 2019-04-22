d45e10ea5031f28a6525665d5b779d9c07dd33df
SirMorfield <joppe.koers@gmail.com>
2019-04-22 21:03:29 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd ~
+
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs -y
+rm nodesource_setup.sh
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
+npm install -g pm2
+
+sudo setcap cap_net_bind_service=+ep /usr/bin/node
+
+git clone https://github.com/SirMorfield/joppekoers.nl.git
+cd joppekoers.nl/
+
+npm install
