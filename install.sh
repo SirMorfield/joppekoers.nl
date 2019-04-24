04125f2cc770e3ed14eb50d849ec3ebef3aa42ce
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2019-04-24 23:02:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,12 @@ sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
 rm nodesource_setup.sh
 
-sudo npm install -g pm2
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo " export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
+source ~/.profile
+
+npm install -g pm2
 
 sudo setcap cap_net_bind_service=+ep /usr/bin/node
 
