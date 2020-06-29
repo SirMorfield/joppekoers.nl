ed116d66cbb01e9f97b072c39c23517279624cec
SirMorfield <joppe.koers@gmail.com>
2020-06-29 18:33:12 +0200

    converting spaces to tabs

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,29 @@
 let options = {
-  title: process.env.NODE_ENV == 'production' ? 'Joppe Koers.nl' : 'localhost',
-  home: 'Home',
-  contact: 'Contact',
-  guestProjects: 'Guests\' projects',
-  wolmolen: 'Wolmolen',
-  partials: [],
-  partialOptions: {}
+	title: process.env.NODE_ENV == 'production' ? 'Joppe Koers.nl' : 'localhost',
+	home: 'Home',
+	contact: 'Contact',
+	guestProjects: 'Guests\' projects',
+	wolmolen: 'Wolmolen',
+	partials: [],
+	partialOptions: {}
 }
 
 exports.home = (req, res) => {
-  let home = options
-  home.partials = ['partials/home.ejs']
-  res.render('index.ejs', home)
+	let home = options
+	home.partials = ['partials/home.ejs']
+	res.render('index.ejs', home)
 }
 
 exports.contact = (req, res) => {
-  let contact = options
-  contact.partials = ['partials/contact.ejs']
-  res.render('index.ejs', contact)
+	let contact = options
+	contact.partials = ['partials/contact.ejs']
+	res.render('index.ejs', contact)
 }
 
 exports.guestProjects = (req, res) => {
-  let guestProjects = options
-  guestProjects.partials = ['partials/guestProjects.ejs']
-  res.render('index.ejs', guestProjects)
+	let guestProjects = options
+	guestProjects.partials = ['partials/guestProjects.ejs']
+	res.render('index.ejs', guestProjects)
 }
 
 exports.standardOptions = options
