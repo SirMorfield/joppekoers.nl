5a5b7b18d2948eb4d8ab6679101524a41e8af8f2
SirMorfield <joppe.koers@gmail.com>
2020-09-27 19:37:48 +0200

    allow multiple files upload on /drop

--- server/db.js
+++ server/db.js
@@ -3,13 +3,6 @@ const path = require('path')
 const env = require(path.join(process.env.root, 'configs/env.json'))
 const exec = require('util').promisify(require('child_process').exec)
 
-function timestamp(d) {
-	const date = d.toISOString().split('T')[0]
-	const time = d.toTimeString().split(' ')[0]
-	const ms = `${d.getMilliseconds()}`.padStart(3, '0')
-	return `${date} ${time}:${ms}`.replace(/\:/g, '.')
-}
-
 let db = {}
 if (process.env.NODE_ENV == 'production') {
 	Object.assign(db, env.drop.db)
@@ -33,33 +26,79 @@ async function DBFull(newFile) {
 	return newSize > db.maxDBSize
 }
 
-async function getSavePath(identifier, filename, fileSize) {
-	if (await DBFull(fileSize))
-		return { error: 'Database is full' }
-	if (db.info[identifier])
-		return { error: 'Identifier already in use' }
-	const saveName = `${timestamp(new Date())}${filename.match(/\.[0-9a-z]+$/i)[0]}`
-	db.info[identifier] = [
-		{
-			path: path.join(db.contentDir, saveName),
-			name: filename,
-			size: fileSize,
+function validateIdentifier(dbContents, identifier) {
+	if (identifier.length == 0) {
+		return { error: 'Identifier too short' }
+	}
+	if (identifier.length > 20) {
+		return { error: 'Identifier and/or filename too long' }
+	}
+	if (identifier.match(/[^\w\d]/)) {
+		return { error: 'Invalid identifier' }
+	}
+	for (const file of dbContents) {
+		if (file.match(/^[\w\d]*-/) == `${identifier}-`) {
+			return { error: 'Identifier already in use' }
 		}
-	]
-	await fs.promises.writeFile(db.dbFile, JSON.stringify(db.info))
-	return db.info[identifier][0].path
+	}
+	return { error: null }
+}
+
+function getSavePath(dbContents, identifier, filename) {
+	let n = 0
+	let dbFilename
+	do {
+		dbFilename = `${identifier}-${n}-${filename}`
+		n++
+	} while (dbContents.includes(filename))
+	return path.join(db.contentDir, dbFilename)
+}
+
+async function saveFiles(files, identifier) {
+	const totalFileSize = files.reduce((acc, cur) => cur.size + acc)
+	if (await DBFull(totalFileSize)) {
+		return { error: 'Database is full' }
+	}
+	const dbContents = await fs.promises.readdir(db.contentDir)
+	const validIdentifier = validateIdentifier(dbContents, identifier)
+	if (validIdentifier.error) return validIdentifier
+
+	for (const file of files) {
+		const dbPath = getSavePath(dbContents, identifier, file.name)
+		try {
+			await file.mv(dbPath)
+		} catch (err) {
+			return { error: `Failed to save file "${file.name}"` }
+		}
+	}
+	return { error: null }
+}
+
+async function getFiles(identifier) {
+	let files = await fs.promises.readdir(db.contentDir)
+	files = files.filter(file => file.match(/^[\w\d]*-/) == `${identifier}-`)
+	files = files.map((file) => {
+		return {
+			path: path.join(db.contentDir, file),
+			name: file.replace(/^[\w\d]*-\d*-/, '')
+		}
+	})
+	return files
 }
 
 const fileUploadSettings = {
 	limits: { fileSize: db.maxFileSize },
 	abortOnLimit: true,
 	useTempFiles: true,
-	tempFileDir: db.tmpDir
+	tempFileDir: db.tmpDir,
+	preserveExtension: true,
+	saveFileNames: true
 }
 
 const fileUpload = require('express-fileupload')
 module.exports = {
 	...db,
-	getSavePath,
+	saveFiles,
+	getFiles,
 	fileUpload: fileUpload(fileUploadSettings)
 }
