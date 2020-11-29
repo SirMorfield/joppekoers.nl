09db2c0819ef63738dce6a3553c7f58476e89a8c
SirMorfield <joppe.koers@gmail.com>
2020-11-30 00:21:04 +0100

    bump nodejs version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~
 
-curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs -y
 rm nodesource_setup.sh
