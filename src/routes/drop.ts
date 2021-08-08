2cf29fd57d4dbcab7fe5ee00eaf6d0c353b845a5
SirMorfield <joppe.koers@gmail.com>
2021-08-08 21:12:36 +0200

    finalizing /drop route

--- src/routes/drop.ts
+++ src/routes/drop.ts
@@ -9,51 +9,55 @@ export function drop(req, res) {
 
 export async function upload(req, res) {
 	let busboy = new Busboy({ headers: req.headers })
-	let files: db.DBFile[] = []
+	let nFilesUploaded = 0
+	const id = db.newID()
 
 	busboy.on('file', async (fieldName, stream, filename, encoding, mimetype) => {
-		let file = await db.newFilePath(filename)
+		let file = await db.newDBFile(filename, id)
 		if (!file) {
 			stream.resume() // aka stream.close()
 			return
 		}
-		files.push(file)
-		console.log(file.path)
+		nFilesUploaded++
 		stream.pipe(fs.createWriteStream(file.path))
 		// stream.on('end', () => { })
 	})
 
 	busboy.on('finish', () => {
-		if (files.length == 0) {
+		if (nFilesUploaded == 0) {
 			res.send('No files uploaded')
+			return
 		}
 
 		const userAgent = (req.get('user-agent')).toLowerCase()
 		if (userAgent.includes('curl') || userAgent.includes('wget')) {
-			res.send(`joppekoers.nl/drop/${files[0]!.id}`)
+			res.send(`joppekoers.nl/drop/${id}`)
 		}
 		else {
-			res.send(`Done, go to <b>joppekoers.nl/drop/${files[0]!.id}</b> to download it`)
+			res.send(`Done, go to <b>joppekoers.nl/drop/${id}</b> to download it`)
 		}
 	})
 	req.pipe(busboy)
 }
 
 export async function download(req, res) {
-	const identifier = req.params.identifier
-	if (identifier.length == 0) {
+	const id = req.params.identifier
+	if (id.length == 0) {
 		res.send('Empty identifier')
 		return
 	}
-	const files: db.DBFile[] = db.db.files[identifier]!
-	if (!files || files.length == 0) {
-		res.send('Identifier not found')
+	const files = db.getFilesByID(id)
+	if (files.length == 0) {
+		res.send('Identifier not found!')
 		return
 	}
 	if (files.length === 1) {
 		res.download(files[0]!.path, files[0]!.name)
 	}
 	else {
-		res.zip(files, `${identifier}.zip`);
+		res.zip({
+			files: files.map(file => { return { path: file.path, name: file.name } }),
+			filename: `${id}.zip`
+		})
 	}
 }
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,10 +1,22 @@
 import * as path from 'path'
 import * as fs from 'fs'
-import * as os from 'os'
-import { promisify } from 'util'
 import { exec } from 'child_process'
+import { PathLike } from 'node:fs'
 const env = require(path.join(process.env['root']!, 'configs/env.json'))
-const execPromise = promisify(exec)
+
+async function execAsync(command): Promise<{ stdout: string, stderr: string }> {
+	return new Promise((resolve, reject) => {
+		exec(`${command}`, (error, stdout, stderr) => {
+			resolve({ stdout, stderr })
+		})
+	})
+}
+
+export interface DBFile {
+	name: string,
+	path: PathLike,
+	id: string
+}
 
 export interface Db {
 	contentDir: string,
@@ -12,36 +24,26 @@ export interface Db {
 	tmpDir: string,
 	maxDBSize: number,
 	maxFileSize: number,
-	files: any
-}
-
-function loadDbConfig(): Db {
-	if (process.env['NODE_ENV'] == 'production' && (os.hostname()).match(/server1/i)) {
-		return env.drop.db
-	}
-	else {
-		return {
-			contentDir: path.join(process.env['root']!, 'server/drop/'),
-			dbFile: path.join(process.env['root']!, 'server/dropDB.json'),
-			tmpDir: '/tmp/',
-			maxDBSize: 1.1e+10,
-			maxFileSize: 1.074e+9,
-			files: {},
-		}
-	}
+	files: DBFile[]
 }
 
 function initDB(): Db {
-	let db: Db = loadDbConfig()
+	let db: Db = env.drop.db
 
-	if (!fs.existsSync(db.contentDir)) fs.mkdirSync(db.contentDir, { recursive: true })
-	if (!fs.existsSync(db.tmpDir)) fs.mkdirSync(db.tmpDir, { recursive: true })
-	if (!fs.existsSync(db.dbFile)) fs.writeFileSync(db.dbFile, '{}')
+	if (!db.contentDir || !db.dbFile || !db.maxDBSize || !db.maxFileSize)
+		console.error('Missing DB options in env file')
+	try {
+		if (!fs.existsSync(db.contentDir)) fs.mkdirSync(db.contentDir, { recursive: true })
+		if (!fs.existsSync(db.tmpDir)) fs.mkdirSync(db.tmpDir, { recursive: true })
+		if (!fs.existsSync(db.dbFile)) fs.writeFileSync(db.dbFile, '[]')
+	} catch (err) {
+		console.error('Cannot create db directories:', err)
+	}
 
 	try {
 		db.files = JSON.parse((fs.readFileSync(db.dbFile)).toString())
 	} catch (err) {
-		db.files = {}
+		db.files = []
 		console.error('Failed to read dbFile ', db.dbFile)
 	}
 	return db
@@ -58,20 +60,18 @@ function logDBinfo(db: Db) {
 		table.filesSaved = `Error reading contentDir`
 	}
 	table.filesInDBfile = 0
-	for (const identifier in db.files) {
-		table.filesInDBfile += db.files[identifier].length
-	}
+
 	table.tmpDir = db.tmpDir
 	table.dbFile = db.dbFile
 	console.table(table)
 }
 
-export let db: Db = initDB()
+const db: Db = initDB()
 
 logDBinfo(db)
 
 export async function DBFull(): Promise<boolean> {
-	const { stdout, stderr } = await execPromise(`du -s -B1 '${db.contentDir}'`)
+	const { stdout, stderr } = await execAsync(`du -s -B1 '${db.contentDir}'`)
 	if (stderr) return true
 	return parseInt(stdout.split(' ')[0]!) > db.maxDBSize
 }
@@ -80,29 +80,61 @@ async function saveDBStatus() {
 	await fs.promises.writeFile(db.dbFile, JSON.stringify(db.files))
 }
 
-export interface DBFile {
-	name: string,
-	path: string,
-	id: string
+function randomNumber(length: number): string {
+	let table = '0123456789'
+	let text = ''
+	while (length-- > 0)
+		text += table.charAt(Math.round(Math.random() * (table.length - 1)))
+	return text
 }
 
-export async function newFilePath(filename: string): Promise<DBFile | null> {
+const reserved: string[] = []
+export function newID(): string {
+	let len = 4
+	let loops = 0
+	let exists
+	let id
+	do {
+		exists = false
+		id = randomNumber(len)
+		for (const file of db.files) {
+			if (file.id == id) {
+				exists = true
+				break
+			}
+		}
+		loops++
+		if (loops == 100) {
+			loops = 0
+			len++
+		}
+	} while (exists || reserved.includes(id))
+	reserved.push(id)
+	return id
+}
+
+export async function newDBFile(filename: string, id: string): Promise<DBFile | null> {
 	if (await DBFull())
 		return null
-	const id = String(Date.now())
-	return {
+
+	const dbFIle = {
 		name: filename,
-		path: path.join(db.contentDir, id),
+		path: path.join(db.contentDir, randomNumber(32)),
 		id,
 	}
+	db.files.push(dbFIle)
+	saveDBStatus()
+	return dbFIle
+}
+
+export function getFilesByID(id: string): DBFile[] {
+	return db.files.filter((file) => file.id === id)
 }
 
 export async function deleteFile(savePath): Promise<void> {
 	if (savePath.indexOf(db.contentDir) == 0) {
 		await fs.promises.unlink(savePath)
 	}
-	for (const identifier in db.files) {
-		db.files[identifier] = db.files[identifier].filter((file) => file.savePath != savePath)
-	}
-	saveDBStatus()
+	db.files = db.files.filter((file) => file.path != savePath)
+	await saveDBStatus()
 }
