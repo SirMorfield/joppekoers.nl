c062b2b05d41f716109510f069fc7cfcd82337ff
SirMorfield <jkoers@student.codam.nl>
2021-12-04 20:32:58 +0100

    delete unused code

--- unfinshedProjects/ip.js
+++
@@ -1,5 +0,0 @@
-function getIpFromReq(req) { // get the client's IP address
-  var bareIP = ":" + ((req.connection.socket && req.connection.socket.remoteAddress)
-    || req.headers["x-forwarded-for"] || req.connection.remoteAddress || "");
-  return (bareIP.match(/:([^:]+)$/) || [])[1] || "127.0.0.1";
-}
