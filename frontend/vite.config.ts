cb1427ba94f3355b40f527f71f3c0dad5e0eba8b
Joppe Koers <joppe@q42.nl>
2022-11-04 11:46:08 +0100

    Serve build through express on `/svelte`

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfigExport } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+const config: UserConfigExport = {
+	plugins: [svelte()],
+	base: '/svelte'
+}
+export default defineConfig(config)
