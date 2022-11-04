e6d5c088e7264ff22119d096e8ec1d87e141380e
Joppe Koers <joppe@q42.nl>
2022-11-04 17:43:33 +0100

    Very basic first setup

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,44 +1,40 @@
 <script lang="ts">
-	import svelteLogo from './assets/svelte.svg'
-	import Counter from './lib/Counter.svelte'
+	import ImageGallery from './lib/ImageGallery.svelte'
 </script>
 
 <main>
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank">
-			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
-	<h1>Vite + Svelte</h1>
-
-	<div class="card">
-		<Counter />
-	</div>
-
-	<p>
-		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte
-		app framework powered by Vite!
-	</p>
-
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+	<ImageGallery loading={'eager'} hover={true}>
+		<img src="/projectImg/wolmolen/thumbnail.jpg" alt="" />
+		<img src="/projectImg/hammer/thumbnail.jpg" alt="" />
+		<img src="/projectImg/arcade-machine/thumbnail.jpg" alt="" />
+		<img src="/projectImg/small-box/thumbnail.jpg" alt="" />
+		<img src="/projectImg/matrix/thumbnail.jpg" alt="" />
+		<img src="/projectImg/audio-cart-storage/thumbnail.jpg" alt="" />
+		<img src="/projectImg/audio-cart-storage-2/thumbnail.jpg" alt="" />
+		<img src="/projectImg/door-frame/thumbnail.jpg" alt="" />
+		<img src="/projectImg/wooden-storage-sofa/thumbnail.jpg" alt="" />
+		<img src="/projectImg/table-legs/thumbnail.jpg" alt="" />
+		<img src="/projectImg/speakers/thumbnail.jpg" alt="" />
+		<img src="/projectImg/sword/thumbnail.jpg" alt="" />
+		<img src="/projectImg/sword2/thumbnail.jpg" alt="" />
+		<img src="/projectImg/rainbarrel-stand/thumbnail.jpg" alt="" />
+		<img src="/projectImg/table/thumbnail.jpg" alt="" />
+		<img src="/projectImg/big-hammer/thumbnail.jpg" alt="" />
+		<img src="/projectImg/knife/thumbnail.jpg" alt="" />
+		<img src="/projectImg/jacob-s-ladder/thumbnail.jpg" alt="" />
+		<img src="/projectImg/boom-mic-stand/thumbnail.jpg" alt="" />
+		<img src="/projectImg/cork-screw/thumbnail.jpg" alt="" />
+		<img src="/projectImg/electric-car/thumbnail.jpg" alt="" />
+		<img src="/projectImg/electric-kettle/thumbnail.jpg" alt="" />
+		<img src="/projectImg/greenhouse/thumbnail.jpg" alt="" />
+		<img src="/projectImg/lab-power-supply/thumbnail.jpg" alt="" />
+		<img src="/projectImg/raid-server/thumbnail.jpg" alt="" />
+		<img src="/projectImg/rattan-doors/thumbnail.jpg" alt="" />
+		<img src="/projectImg/rpi-remote-backup/thumbnail.jpg" alt="" />
+		<img src="/projectImg/screwdriver-holder/thumbnail.jpg" alt="" />
+		<img src="/projectImg/small-car/thumbnail.jpg" alt="" />
+		<img src="/projectImg/suskast/thumbnail.jpg" alt="" />
+		<img src="/projectImg/wheel-respoke/thumbnail.jpg" alt="" />
+		<img src="/projectImg/wheel-respoke-2/thumbnail.jpg" alt="" />
+	</ImageGallery>
 </main>
-
-<style>
-	.logo {
-		height: 6em;
-		padding: 1.5em;
-		will-change: filter;
-	}
-	.logo:hover {
-		filter: drop-shadow(0 0 2em #646cffaa);
-	}
-	.logo.svelte:hover {
-		filter: drop-shadow(0 0 2em #ff3e00aa);
-	}
-	.read-the-docs {
-		color: #888;
-	}
-</style>
