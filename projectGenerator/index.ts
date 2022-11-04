19b6b8a4b98565ebabad566f32878d5dba426af8
Joppe Koers <joppe@q42.nl>
2022-11-04 19:49:55 +0100

    prettyfy projectGenerator

--- projectGenerator/index.ts
+++ projectGenerator/index.ts
@@ -1,10 +1,13 @@
 import fs from 'fs-extra'
 import path from 'path'
-import { Image, Path, Project, exec, createThumbnail, imageSize, hasThumbnail, exit, Job, FileName } from "./util"
+import { Image, Path, Project, exec, createThumbnail, imageSize, hasThumbnail, exit, Job, FileName } from './util'
 import { default as sanitizeFilename } from 'sanitize-filename'
 
 function sanatize(path: string): string {
-	return path.split('/').map(name => sanitizeFilename(name.replace(/\s/g, '-').toLowerCase())).join('/')
+	return path
+		.split('/')
+		.map((name) => sanitizeFilename(name.replace(/\s/g, '-').toLowerCase()))
+		.join('/')
 }
 
 const JPEG_QUALITY = 50
@@ -19,8 +22,7 @@ fs.mkdirSync(outputPath, { recursive: true })
 // const inputPath: Path = path.join(__dirname, '../public/img/projectImg/')
 
 function normalizeName(name: FileName, index: number) {
-	if (index > 99)
-		throw new Error('Index too large')
+	if (index > 99) throw new Error('Index too large')
 	return `${index.toString().padStart(2, '0')}${path.extname(name)}`
 }
 
@@ -34,14 +36,13 @@ async function processImage(image: Path, output: Path, index: number): Promise<I
 	// TODO display sizes
 	const { stderr } = await exec(`jpegoptim --max=${JPEG_QUALITY} ${newPath}`)
 	// console.log(stdout.replace(/\n+$/, ''))
-	if (stderr)
-		throw new Error(stderr)
+	if (stderr) throw new Error(stderr)
 
 	return {
 		name: newName,
 		path: newPath,
 		width: dimensions.width,
-		height: dimensions.height
+		height: dimensions.height,
 	}
 }
 
@@ -50,10 +51,9 @@ async function runJob(job: Job): Promise<Image[]> {
 	await fs.emptyDir(job.output)
 
 	const imageDb: Image[] = []
-	let i = 0;
+	let i = 0
 	for (const image of job.imgs) {
-		if (image.match(/\/thumbnail/))
-			continue
+		if (image.match(/\/thumbnail/)) continue
 
 		imageDb.push(await processImage(image, job.output, i++))
 	}
@@ -70,26 +70,15 @@ async function runJob(job: Job): Promise<Image[]> {
 async function generateProjectFromJob(job: Job): Promise<Project> {
 	const images = await runJob(job)
 	const res = {
-		imgs: images.map(image => ({ src: image.name, w: image.width, h: image.height })),
-		root: path.join('/img/projectImg/', job.id) + '/'
+		imgs: images.map((image) => ({ src: image.name, w: image.width, h: image.height })),
+		root: path.join('/img/projectImg/', job.id) + '/',
 	}
 	// console.log(`Done: ${projectID}\n`)
 	return res
 }
 
-async function installProjects(projectsDb: { [key: string]: Project }): Promise<void> {
-	const projects = JSON.stringify(projectsDb)
-		.replace(/\"src\"\:/g, 'src:')
-		.replace(/\"imgs\"\:/g, 'imgs:')
-		.replace(/\"root\"\:/g, 'root:')
-		.replace(/\"w\"\:/g, 'w:')
-		.replace(/\"h\"\:/g, 'h:')
-
-	const publicOpenPopup = (await fs.promises.readFile(path.join(__dirname, 'openPopup.txt')))
-		.toString()
-		.replace('// projectsPlaceholder', `const projects = ${projects}`)
-
-	await fs.promises.writeFile(path.join(__dirname, '../public/js/openPopup.js'), publicOpenPopup)
+async function installProjects(projects: Project[]): Promise<void> {
+	console.log(JSON.stringify(projects))
 }
 
 // Generate TODOs
@@ -103,8 +92,7 @@ async function getJobs(inputsPath: Path): Promise<Job[]> {
 			.map((img) => path.join(inputPath, img))
 			.filter((name) => {
 				const isImage = !!name.match(/\.jpg$/)
-				if (!isImage)
-					console.log(`WARNING: ignoring non-image file ${name}`)
+				if (!isImage) console.log(`WARNING: ignoring non-image file ${name}`)
 				return isImage
 			})
 		return {
@@ -116,22 +104,22 @@ async function getJobs(inputsPath: Path): Promise<Job[]> {
 	return Promise.all(inputs)
 }
 
-(async () => {
+;(async () => {
 	const jobs = await getJobs(inputPath)
-	const projectsDb: { [key: string]: Project } = {}
+	const projects: Project[] = []
 
 	for (const job of jobs) {
 		console.log(`Project ${job.id}`)
-		if (projectsDb[job.id])
-			exit(`Project ${job.id} already exists`)
+		if (projects[job.id]) exit(`Project ${job.id} already exists`)
 
-		projectsDb[job.id] = await generateProjectFromJob(job)
+		projects.push(await generateProjectFromJob(job))
 	}
 
-	await installProjects(projectsDb)
+	await installProjects(projects)
 
-	for (const project of Object.keys(projectsDb)) {
-		console.log(`<div class="project block scale-up"><img src="/img/projectImg/${project}/thumbnail.jpg" onclick="openPopup('${project}')" class="lazyload projectImg"></div>`)
+	for (const project of Object.keys(projects)) {
+		// prettier-ignore
+		console.log(`<div class="project block scale-up"><img src="/img/projectImg/${project}/thumbnail.jpg" onclick="openPopup('${project}')" class="lazyload projectImg"></div>`,)
 	}
 })()
 
--- projectGenerator/util.ts
+++ projectGenerator/util.ts
@@ -3,23 +3,22 @@ export type FileName = string
 
 export interface Image {
 	name: FileName // eg. 01.jpg
-	path: Path      // eg. /path/to/01.jpg
+	path: Path // eg. /path/to/01.jpg
 	width: number
 	height: number
 }
 
 export interface Job {
-	id: string   // id / slug of the project
+	id: string // id / slug of the project
 	imgs: Path[] // the full image paths of the job
 	output: Path
 }
 
 export interface Project {
 	// thumbnail: Image[]
-	imgs: { src: string, w: number, h: number }[]
+	imgs: { src: string; w: number; h: number }[]
 	root: Path
 }
-
 
 export const exec = require('util').promisify(require('child_process').exec)
 
@@ -28,11 +27,11 @@ export async function createThumbnail(imagePath: string, thumbnailPath: string):
 	if (stderr) console.error(stderr)
 }
 
-export async function imageSize(path: string): Promise<{ width: number, height: number }> {
+export async function imageSize(path: string): Promise<{ width: number; height: number }> {
 	const identity = await exec(`identify -format "%wx%h" '${path}'`)
 	if (identity.stderr) console.error(identity.stderr)
 	identity.stdout = identity.stdout.trim()
-	identity.stdout = identity.stdout.split("x")
+	identity.stdout = identity.stdout.split('x')
 	const result = {
 		width: parseInt(identity.stdout[0]),
 		height: parseInt(identity.stdout[1]),
@@ -41,13 +40,10 @@ export async function imageSize(path: string): Promise<{ width: number, height: 
 	// the data is stored in portrait mode, but the photo was taken in vertical
 	try {
 		const { stdout } = await exec(`exif -t Orientation -m '${path}'`)
-		if (
-			stdout.match(/Right\-top/m) ||
-			stdout.match(/Left\-bottom/m)
-		) {
-			[result.width, result.height] = [result.height, result.width]
+		if (stdout.match(/Right\-top/m) || stdout.match(/Left\-bottom/m)) {
+			;[result.width, result.height] = [result.height, result.width]
 		}
-	} catch (err) { }
+	} catch (err) {}
 	return result
 }
 
