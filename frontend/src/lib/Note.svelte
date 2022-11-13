7b1144e290aa1fd864cb1ad644398ab287458a41
Joppe Koers <joppe@q42.nl>
2022-11-13 13:17:48 +0100

    Add note

---
+++ frontend/src/lib/Note.svelte
@@ -0,0 +1,16 @@
+<div class="note">
+	<slot />
+</div>
+
+<style>
+	.note {
+		background-color: #f0f0f0;
+		padding: 1rem;
+		border-radius: 0.5rem;
+		color: black;
+	}
+	.note a {
+		text-decoration: underline;
+		color: black;
+	}
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,12 +2,19 @@
 	import ImageGallery from '$lib/ImageGallery.svelte'
 	import Root from '$lib/Root.svelte'
 	import ProjectCard from '$lib/ProjectCard.svelte'
-
+	import Note from '$lib/Note.svelte'
 </script>
 
 <main>
 	<Root>
 		<ImageGallery>
+			<Note>
+				Hello and welcome to Joppe Koers' website. This is where you'll find my portfolio, personal projects and
+				a mix of programming related stuff. Since I can remember I've always wanted to make stuff. To accomplish
+				this I acquired a number of skills of which you can see the result below. Do you want me to make you
+				something? See the projects below to see what I'm capable of. You can contact me form the
+				<a href="/contact" class="internalLink">/contact</a> page.
+			</Note>
 			<ProjectCard id="wolmolen" />
 			<ProjectCard id="hammer" />
 			<ProjectCard id="arcade-machine" />
@@ -43,3 +50,10 @@
 		</ImageGallery>
 	</Root>
 </main>
+
+<style>
+	.internalLink {
+		text-decoration: underline;
+		color: black;
+	}
+</style>
