48aae391052294ab5dc7ab64182901f0a68831bb
Joppe Koers <joppe@joppekoers.nl>
2023-05-18 23:55:20 +0200

    Use css

--- frontend/src/lib/ImageGallery.svelte
+++ frontend/src/lib/ImageGallery.svelte
@@ -1,70 +1,63 @@
-<!-- From: https://github.com/berkinakkaya/svelte-image-gallery#readme -->
-<script lang="ts">
-	import { onMount, tick } from 'svelte'
-
-	export let gap = 10
-	export let maxColumnWidth = 300
-
-	let slotHolder: HTMLDivElement | null = null
-
-	let galleryWidth = 0
-	let columnCount = 0
-	let columns: (HTMLDivElement | null)[] = []
-
-	$: columnCount = Math.floor(galleryWidth / maxColumnWidth) || 1
-	$: columnCount && draw()
-	$: galleryStyle = `grid-template-columns: repeat(${columnCount}, 1fr); --gap: ${gap}px`
-
-	async function draw() {
-		await tick()
-		if (!slotHolder) return
-
-		columns = Array(columnCount).fill(null)
-		await tick() // waiting for colums to be created
-
-		// Fill the columns with image URLs
-		let i = 0
-		for (const node of slotHolder.childNodes) {
-			if (!(node instanceof HTMLElement)) continue
-			const column = i++ % columnCount
-			columns[column].appendChild(node)
-		}
-	}
-
-	onMount(async () => {
-		await draw()
-	})
+<script>
+	import Note from '$lib/Note.svelte'
+	import ProjectCard from '$lib/ProjectCard.svelte'
 </script>
 
-<div id="imported" bind:this={slotHolder}>
-	<slot />
-</div>
+<!-- From: https://github.com/berkinakkaya/svelte-image-gallery#readme -->
 
-<div id="gallery" bind:clientWidth={galleryWidth} style={galleryStyle}>
-	{#each columns as _, i}
-		<div class="column" bind:this={columns[i]} />
-	{/each}
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+	<div class="grid gap-4 content-start">
+		<Note>
+			Hello and welcome to Joppe Koers' website. This is where you'll find my portfolio, personal projects and a
+			mix of programming related stuff. Since I can remember I've always wanted to make stuff. To accomplish this
+			I acquired a number of skills of which you can see the result below. Do you want me to make you something?
+			See the projects below to see what I'm capable of. You can contact me form the
+			<a href="/contact" class="internalLink">/contact</a> page.
+		</Note>
+		<ProjectCard id="small-box" />
+		<ProjectCard id="door-frame" />
+		<ProjectCard id="sword" />
+		<ProjectCard id="big-hammer" />
+		<ProjectCard id="cork-screw" />
+		<ProjectCard id="lab-power-supply" />
+		<ProjectCard id="screwdriver-holder" />
+		<ProjectCard id="wheel-respoke-2" />
+	</div>
+	<div class="grid gap-4 content-start">
+		<ProjectCard id="wolmolen" />
+		<ProjectCard id="matrix" />
+		<ProjectCard id="wooden-storage-sofa" />
+		<ProjectCard id="sword2" />
+		<ProjectCard id="knife" />
+		<ProjectCard id="electric-car" />
+		<ProjectCard id="raid-server" />
+		<ProjectCard id="small-car" />
+	</div>
+	<div class="grid gap-4 content-start">
+		<ProjectCard id="hammer" />
+		<ProjectCard id="audio-cart-storage" />
+		<ProjectCard id="table-legs" />
+		<ProjectCard id="rainbarrel-stand" />
+		<ProjectCard id="jacob-s-ladder" />
+		<ProjectCard id="electric-kettle" />
+		<ProjectCard id="rattan-doors" />
+		<ProjectCard id="suskast" />
+	</div>
+	<div class="grid gap-4 content-start">
+		<ProjectCard id="arcade-machine" />
+		<ProjectCard id="audio-cart-storage-2" />
+		<ProjectCard id="speakers" />
+		<ProjectCard id="table" />
+		<ProjectCard id="boom-mic-stand" />
+		<ProjectCard id="greenhouse" />
+		<ProjectCard id="rpi-remote-backup" />
+		<ProjectCard id="wheel-respoke" />
+	</div>
 </div>
 
 <style>
-	#imported {
-		display: none;
-	}
-
-	#gallery {
-		width: 100%;
-		display: grid;
-		gap: var(--gap);
-	}
-	#gallery .column {
-		display: flex;
-		flex-direction: column;
-	}
-	#gallery .column * {
-		width: 100%;
-		margin-top: var(--gap);
-	}
-	#gallery .column *:nth-child(1) {
-		margin-top: 0;
+	.internalLink {
+		text-decoration: underline;
+		color: black;
 	}
 </style>
