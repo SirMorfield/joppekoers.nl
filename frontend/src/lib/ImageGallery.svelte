dc6bd7774a4535e4f98dba68266d6c84b957048d
Joppe Koers <joppe@joppekoers.nl>
2023-05-28 14:00:57 +0200

    Handle resize

--- frontend/src/lib/ImageGallery.svelte
+++ frontend/src/lib/ImageGallery.svelte
@@ -1,6 +1,6 @@
 <!-- From: https://github.com/berkinakkaya/svelte-image-gallery#readme -->
 <script lang="ts">
-	import { onMount } from 'svelte'
+	import { onMount, tick } from 'svelte'
 
 	export let gap = 10
 	export const maxColumnCount = 4
@@ -14,29 +14,39 @@
 	const columnStyles: string[] = Array(maxColumnCount).fill(shownColumStyle)
 
 	let galleryWidth = 0
+	let previousGalleryWidth = galleryWidth + 1 // force draw on mount
 	let galleryStyle = ''
 
-	$: columnCount = Math.floor(galleryWidth / maxColumnWidth) || 1
+	$: columnCount = Math.min(Math.floor(galleryWidth / maxColumnWidth) || 1, maxColumnCount)
 	$: columnCount && draw()
 	$: galleryStyle = `grid-template-columns: repeat(${columnCount}, 1fr); --gap: ${gap}px`
 
 	async function draw() {
+		await tick()
 		if (!slotHolder) return
+		if (previousGalleryWidth === galleryWidth) return
+
+		let found = false
+		do {
+			found = false
+			for (const column of columns) {
+				if (column.firstElementChild) {
+					slotHolder.appendChild(column.firstElementChild)
+					found = true
+				}
+			}
+		} while (found)
 
 		let i = 0
-		for (const node of slotHolder.childNodes) {
-			if (!(node instanceof HTMLElement)) continue
-			const column = i++ % columns.length
-			columns[column].appendChild(node)
+		while (slotHolder.firstElementChild) {
+			const column = i++ % columnCount
+			columns[column].appendChild(slotHolder.firstElementChild)
 		}
+		previousGalleryWidth = galleryWidth
 	}
 
 	onMount(async () => {
 		await draw()
-		setTimeout(() => {
-			galleryStyle = `grid-template-columns: repeat(${columns.length - 1}, 1fr); --gap: ${gap}px`
-			columnStyles[columns.length - 1] = hiddenColumStyle
-		}, 500)
 	})
 </script>
 
