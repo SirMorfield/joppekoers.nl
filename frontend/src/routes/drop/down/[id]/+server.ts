eb527e5c6b983d973eac6930858be93c2387647b
Joppe Koers <joppe@joppekoers.nl>
2023-06-07 01:16:26 +0200

    Validate input

--- frontend/src/routes/drop/down/[id]/+server.ts
+++ frontend/src/routes/drop/down/[id]/+server.ts
@@ -1,10 +1,14 @@
 import type { RequestHandler } from '@sveltejs/kit'
 import fs from 'fs/promises'
 import { getFileById, readableToReadStream } from '../../_server'
+import { z } from 'zod'
 
 export const GET: RequestHandler = async ({ setHeaders, params }) => {
-	const id = params.id
-	const file = await getFileById(id)
+	const id = await z.string().min(1).safeParseAsync(params.id)
+	if (!id.success) {
+		return new Response('Invalid file id', { status: 400 })
+	}
+	const file = await getFileById(id.data)
 	if (!file) {
 		return new Response('File not found', { status: 404 })
 	}
