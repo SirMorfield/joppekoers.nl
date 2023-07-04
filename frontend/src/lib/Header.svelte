df57a15125639e9dd9f56961aea7219a45c57129
Joppe Koers <joppe@joppekoers.nl>
2023-07-04 22:53:29 +0200

    Make header responsive

--- frontend/src/lib/Header.svelte
+++ frontend/src/lib/Header.svelte
@@ -1,51 +1,60 @@
 <script lang="ts">
 	import logo from '../assets/logo.svg'
+
+	let navContent: HTMLDivElement | undefined = undefined
 </script>
 
-<div id="menu">
-	<!-- <a href="/" style="heigth: 100%"> -->
-	<img src={logo} id="top-logo" alt="logo" width="1217" height="250" />
-	<!-- </a> -->
+<nav class="flex items-center justify-between flex-wrap py-6 px-5 w-full z-10 top-0 fixed">
+	<div class="flex items-center flex-shrink-0 text-white mr-6">
+		<a class="text-white no-underline hover:text-white hover:no-underline" href="/">
+			<img src={logo} class="h-10 w-auto" alt="logo" width="1217" height="250" />
+			<!-- <span class="text-2xl pl-2"><i class="em em-grinning" /> Brand McBrandface</span> -->
+		</a>
+	</div>
 
-	<a href="/">
-		<div class="menuItem">Home</div>
-	</a>
+	<div class="block lg:hidden">
+		<button
+			id="nav-toggle"
+			class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
+			on:click={() => navContent.classList.toggle('hidden')}>
+			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+				<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+			</svg>
+		</button>
+	</div>
 
-	<a href="/contact">
-		<div class="menuItem">Contact</div>
-	</a>
+	<div
+		class="w-full flex-grow lg:items-start lg:w-auto hidden lg:block lg:pt-0 bg-red-600"
+		id="nav-content"
+		bind:this={navContent}>
+		<ul class="list-reset lg:flex justify-start flex-1 items-start">
+			<li class="mr-3">
+				<a class="link" href="/">Home</a>
+			</li>
+			<li class="mr-3">
+				<a class="link" href="/contact">Contact</a>
+			</li>
+			<li class="mr-3">
+				<a class="link" href="/">cv </a>
+			</li>
+		</ul>
+	</div>
+</nav>
 
-	<a href="/cv">
-		<div class="menuItem">cv</div>
-	</a>
-</div>
+<div class="container shadow-lg mx-auto bg-white mt-20 md:mt-18" />
 
 <style>
-	a {
+	.link {
+		display: inline-block;
+		padding-top: 0.5rem;
+		padding-bottom: 0.5rem;
+		padding-left: 1rem;
+		padding-right: 1rem;
+		color: #4b5563;
 		text-decoration: none;
-		color: #dddddd;
-	}
-	a:hover {
-		text-decoration: underline;
-	}
-	#top-logo {
-		/* max-width: 100%; */
-		max-height: 100%;
-		width: auto;
-		object-fit: contain;
-	}
-	#menu {
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-		align-items: center;
-		height: 70px;
-		padding: 20px 0;
 	}
 
-	.menuItem {
-		font-size: 30px;
-		padding: 0px 20px;
-		cursor: pointer;
+	.link:hover {
+		color: #e5e7eb;
 	}
 </style>
