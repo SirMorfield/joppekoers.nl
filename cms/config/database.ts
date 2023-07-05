cd573d6c013791c84668c0377b4f8d2fe85c16a2
Joppe Koers <joppe@joppekoers.nl>
2023-07-05 19:32:18 +0200

    Always listen to all

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -7,7 +7,7 @@ export default ({ env }) => {
 		mysql: {
 			connection: {
 				connectionString: env('DATABASE_URL'),
-				host: env('DATABASE_HOST', 'localhost'),
+				host: env('DATABASE_HOST', '0.0.0.0'),
 				port: env.int('DATABASE_PORT', 3306),
 				database: env('DATABASE_NAME', 'strapi'),
 				user: env('DATABASE_USERNAME', 'strapi'),
@@ -28,7 +28,7 @@ export default ({ env }) => {
 		},
 		mysql2: {
 			connection: {
-				host: env('DATABASE_HOST', 'localhost'),
+				host: env('DATABASE_HOST', '0.0.0.0'),
 				port: env.int('DATABASE_PORT', 3306),
 				database: env('DATABASE_NAME', 'strapi'),
 				user: env('DATABASE_USERNAME', 'strapi'),
@@ -50,7 +50,7 @@ export default ({ env }) => {
 		postgres: {
 			connection: {
 				connectionString: env('DATABASE_URL'),
-				host: env('DATABASE_HOST', 'localhost'),
+				host: env('DATABASE_HOST', '0.0.0.0'),
 				port: env.int('DATABASE_PORT', 5432),
 				database: env('DATABASE_NAME', 'strapi'),
 				user: env('DATABASE_USERNAME', 'strapi'),
