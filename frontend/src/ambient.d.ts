257f954adc23f8bc65523a091236c154008ce4a8
Joppe Koers <joppe@joppekoers.nl>
2023-07-12 22:09:19 +0200

    Add `svelte-img`

---
+++ frontend/src/ambient.d.ts
@@ -0,0 +1,5 @@
+// https://github.com/JonasKruckenberg/imagetools/issues/160
+declare module './assets/*' {
+	const meta: object[]
+	export default meta
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { searchForWorkspaceRoot, type UserConfig } from 'vite'
+import { imagetools } from '@zerodevx/svelte-img/vite'
 
 const config: UserConfig = {
 	plugins: [
 		sveltekit(),
+		imagetools(),
 		{
 			name: 'filepath',
 			transform(code, id) {
