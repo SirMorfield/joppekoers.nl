1b16b76ad21d14549bbc3d343e9b3935d4528175
Joppe Koers <joppe@joppekoers.nl>
2023-08-02 17:06:23 +0200

    Create custom middleware

--- cmsj/src/app.ts
+++ cmsj/src/app.ts
@@ -1,6 +1,8 @@
-import { IPXOptions, createIPX, createIPXMiddleware } from 'ipx'
-import express from 'express'
+import etag from 'etag'
+import express, { Request, Response } from 'express'
 import fs from 'fs'
+import { createIPX } from 'ipx'
+import { hash } from 'ohash'
 import path from 'path'
 import { env } from './env'
 
@@ -12,7 +14,7 @@ type Project = {
 const opt = {
 	dir: `${__dirname}/../projects`,
 	maxAge: 60 * 60 * 24 * 100,
-} as const satisfies Partial<IPXOptions>
+} as const
 
 async function generateIndex(): Promise<Project[]> {
 	const files = await fs.promises.readdir(opt.dir)
@@ -34,9 +36,101 @@ async function generateIndex(): Promise<Project[]> {
 	return projects
 }
 
+if (!fs.existsSync(env.cacheDir)) {
+	fs.mkdirSync(env.cacheDir, { recursive: true })
+}
+
 const ipx = createIPX(opt)
+
+// inspired by https://github.com/strapi-community/strapi-plugin-local-image-sharp
+async function createMiddleware(req: Request, res: Response) {
+	const objectHash = hash({ id: req.path, ...req.query })
+	const tmpFilePath = path.join(env.cacheDir, `${objectHash}.raw`)
+	const tmpTypePath = path.join(env.cacheDir, `${objectHash}.mime`)
+	const tmpEtagPath = path.join(env.cacheDir, `${objectHash}.etag`)
+
+	if (fs.existsSync(tmpFilePath)) {
+		try {
+			const [type, etag] = await Promise.all([
+				fs.promises.readFile(tmpTypePath, 'utf-8'),
+				fs.promises.readFile(tmpEtagPath, 'utf-8'),
+			])
+
+			res.set('ETag', etag)
+			if (etag && req.headers['if-none-match'] === etag) {
+				res.sendStatus(304)
+				return
+			}
+
+			res.set('Cache-Control', `max-age=${env.maxAge}, public, s-maxage=${env.maxAge}`)
+
+			if (type) {
+				res.set('Content-Type', type)
+			}
+
+			const stream = fs.createReadStream(tmpFilePath)
+			stream.pipe(res)
+
+			return
+		} catch (error) {
+			console.error(new Error(error as string))
+		}
+	}
+
+	const img = ipx(req.path, req.query)
+	try {
+		const src = await img.src()
+
+		// Caching headers
+		if (src.mtime) {
+			if (req.headers['if-modified-since']) {
+				if (new Date(req.headers['if-modified-since']) >= src.mtime) {
+					res.sendStatus(304)
+					return
+				}
+			}
+			res.set('Last-Modified', `${+src.mtime}`)
+		}
+
+		const maxAge = src.maxAge ?? env.maxAge
+
+		if (maxAge) {
+			res.set('Cache-Control', `max-age=${maxAge}, public, s-maxage=${maxAge}`)
+		}
+
+		// Get converted image
+		const { data, format } = await img.data()
+		const etagV = etag(data)
+
+		void Promise.all([
+			fs.promises.writeFile(tmpTypePath, `image/${format}`, 'utf-8'),
+			fs.promises.writeFile(tmpEtagPath, etagV, 'utf-8'),
+			fs.promises.writeFile(tmpFilePath, data),
+		]).catch(error => {
+			console.error(new Error(error))
+		})
+
+		res.set('ETag', etagV)
+		if (etagV && req.headers['if-none-match'] === etagV) {
+			res.sendStatus(304)
+			return
+		}
+
+		if (format) {
+			res.set('Content-Type', `image/${format}`)
+		}
+
+		res.send(data)
+	} catch (error) {
+		console.log('err', new Error(error as string))
+		res.sendStatus(500)
+		// @ts-ignore
+		res.send(error.message ?? error)
+	}
+}
+
 const app = express()
-app.use('/image', createIPXMiddleware(ipx))
+app.use('/projects', createMiddleware)
 app.get('/project-index', async (_, res) => {
 	const projects = await generateIndex()
 	res.json(projects)
--- cmsj/src/env.ts
+++ cmsj/src/env.ts
@@ -4,6 +4,9 @@ const Env = z.object({
 	dev: z.boolean(),
 	port: z.number().min(0).max(65535),
 	cmsUrl: z.string().url(),
+	projects: z.string(),
+	cacheDir: z.string(),
+	maxAge: z.number().min(0),
 })
 export type Env = z.infer<typeof Env>
 
@@ -13,6 +16,9 @@ export const env = {
 	dev,
 	port,
 	cmsUrl: dev ? `http://127.0.0.1:${port}` : 'https://cms.joppekoers.nl',
+	projects: `${__dirname}/../projects`,
+	cacheDir: `${__dirname}/../projects/cache`,
+	maxAge: 60 * 60 * 24 * 100,
 } as const satisfies Env
 
 Env.parse(env)
