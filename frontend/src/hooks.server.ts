52fb5a7aa9d5547aac851b8a7c4750f40f9f999e
Joppe Koers <joppe@joppekoers.nl>
2023-08-07 14:13:23 +0200

    Better hook

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,6 +1,19 @@
 import type { HandleServerError } from '@sveltejs/kit'
 
+function toString(e: unknown) {
+	if (e instanceof Error) {
+		return e.message
+	}
+	return String(e)
+}
 export const handleError: HandleServerError = a => {
+	if (a.event.route.id) {
+		console.error(a.error)
+		return {
+			status: 500,
+			message: toString(a.error),
+		}
+	}
 	const message = `404: page ${a.event.url.pathname} not found`
 	console.log(message)
 	return { message }
