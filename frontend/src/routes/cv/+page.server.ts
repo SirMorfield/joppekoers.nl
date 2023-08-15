f5b78d4e4c80f0916a806f2fb171a7d94e626106
SirMorfield <joppe@joppekoers.nl>
2023-08-15 13:24:21 +0200

    Use cms for static files

--- frontend/src/routes/cv/+page.server.ts
+++ frontend/src/routes/cv/+page.server.ts
@@ -1,6 +1,7 @@
+import { env } from '$root/env'
 import type { PageServerLoad } from './$types'
 import { redirect } from '@sveltejs/kit'
 
 export const load: PageServerLoad = () => {
-	throw redirect(302, '/media/CV Joppe Koers.pdf')
+	throw redirect(302, new URL('/media/CV Joppe Koers.pdf', env.cmsUrl).toString())
 }
